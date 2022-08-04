1353a62c7d70e2e26045fba441e72d7079826682
Steven <91740061+Syaga009@users.noreply.github.com>
2022-08-04 13:59:27 -0500

    Update actualizar.php

--- actualizar.php
+++ actualizar.php
@@ -3,12 +3,12 @@
 header("Access-Control-Allow-Origin: *");
 require_once __DIR__ . '/vendor/autoload.php';
 // Crear Cliente---------------------------------------------------------------------
-$client = new MongoDB\Client('mongodb+srv://TBBLuxari:DMc53jwH5CIQAryP@prueba-puntos.veb9sop.mongodb.net/?retryWrites=true&w=majority');
+$client = new MongoDB\Client('mongodb+srv://AdnTraining:DFkARIj7KUmhC4qr@adn.zbruols.mongodb.net/?retryWrites=true&w=majority');
 
 // Traet Base de datos---------------------------------------------------------------
-$database = $client->Sesiones;
+$database = $client->Sesion;
 // Crear o Traer coleccion-----------------------------------------------------------
-$collection2 = $database->UsuariosYagama;
+$collection2 = $database->Yagama;
 
 $PuntajeUnity = $_GET['PuntajeUnity'];
 $CorreoUnity = $_GET['CorreoUnity'];
