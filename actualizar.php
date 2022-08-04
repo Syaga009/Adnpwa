aeeba712f3bb30577e7ff0d27ca41ff7222a44c2
Steven <91740061+Syaga009@users.noreply.github.com>
2022-08-04 13:41:24 -0500

    Add files via upload

---
+++ actualizar.php
@@ -0,0 +1,32 @@
+<?php 
+//(ACTUALIZAR)
+header("Access-Control-Allow-Origin: *");
+require_once __DIR__ . '/vendor/autoload.php';
+// Crear Cliente---------------------------------------------------------------------
+$client = new MongoDB\Client('mongodb+srv://TBBLuxari:DMc53jwH5CIQAryP@prueba-puntos.veb9sop.mongodb.net/?retryWrites=true&w=majority');
+
+// Traet Base de datos---------------------------------------------------------------
+$database = $client->Sesiones;
+// Crear o Traer coleccion-----------------------------------------------------------
+$collection2 = $database->UsuariosYagama;
+
+$PuntajeUnity = $_GET['PuntajeUnity'];
+$CorreoUnity = $_GET['CorreoUnity'];
+$TiempoUnity = $_GET['TiempoUnity'];
+
+// Actualizar un dato ---------------------------------------------------------------
+
+$filtro = ['CORREO' => $CorreoUnity];
+$update = ['$set' => ['PUNTAJE' => intval($PuntajeUnity) , 'INTENTO' => $TiempoUnity]];
+
+$Actualizar = $collection2->updateOne($filtro,$update);
+
+echo 'Actualizado'
+
+/*
+    Se actualizo o no se actualizo
+*/ 
+?>
+
+
+
