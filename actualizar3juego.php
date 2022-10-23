e84cdfa050f3a9619f3bf9bbbe19c46f151fb273
Steven <91740061+Syaga009@users.noreply.github.com>
2022-10-23 13:13:26 -0500

    Create actualizar3juego.php

---
+++ actualizar3juego.php
@@ -0,0 +1,28 @@
+<?php 
+//(ACTUALIZAR)
+header("Access-Control-Allow-Origin: *");
+require_once __DIR__ . '/vendor/autoload.php';
+// Crear Cliente---------------------------------------------------------------------
+$client = new MongoDB\Client('mongodb+srv://TBBLuxari:DMc53jwH5CIQAryP@prueba-puntos.veb9sop.mongodb.net/?retryWrites=true&w=majority');
+
+// Traet Base de datos---------------------------------------------------------------
+$database = $client->Sesiones;
+// Crear o Traer coleccion-----------------------------------------------------------
+$collection2 = $database->usuariostunjas;
+
+$PuntajeUnity = $_GET['PuntajeUnity'];
+$CorreoUnity = $_GET['CorreoUnity'];
+$MetaUnity = $_GET['Puntos3'];
+// Actualizar un dato ---------------------------------------------------------------
+
+$filtro = ['email' => $CorreoUnity];
+$update = ['$set' => ['puntos3' => strval($MetaUnity)]];
+
+$Actualizar = $collection2->updateOne($filtro,$update);
+
+echo 'Actualizado'
+
+/*
+    Se actualizo o no se actualizo
+*/ 
+?>
