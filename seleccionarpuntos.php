c91997ff71fe6cd90a7d7644c9023aab6d1f115d
Steven <91740061+Syaga009@users.noreply.github.com>
2022-10-27 13:07:12 -0500

    Update seleccionarpuntos.php

--- seleccionarpuntos.php
+++ seleccionarpuntos.php
@@ -20,7 +20,7 @@ $correo = $collection2->find($filtro);
 //$Actualizar = $collection2->updateOne($filtro,$update);
 //$result = $collection2->find(array(), array('projection' => array('name' => 1, '_id' => 1)));
 echo 'Actualizado';
-echo $correo;
+echo '$correo';
 
 /*
     Se actualizo o no se actualizo
