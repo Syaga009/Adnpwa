77a736d81abfa752668d5c9bf09a59d01ca36104
Steven <91740061+Syaga009@users.noreply.github.com>
2022-10-27 13:47:11 -0500

    c

--- seleccionarpuntos.php
+++ seleccionarpuntos.php
@@ -20,7 +20,9 @@ $filtro = ['email' => $Email];
 //$Actualizar = $collection2->updateOne($filtro,$update);
 //$result = $collection2->find(array(), array('projection' => array('nombre' => 'Pascual', '_id' => '633f7bb9543bc9f3d8b635b0')));
 $result = $collection2->find(array('email' => 'adntraining'));
-
+foreach ($result as $document) {
+	echo $document['email'] . " " . $document['nombre'] . '</br>';
+}
 echo 'este es el resultado ';
 echo $result;
 
