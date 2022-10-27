1651a2aa2c8a67cf21b70e20f5d01d5dd5e6a5c2
Steven <91740061+Syaga009@users.noreply.github.com>
2022-10-27 13:45:42 -0500

    c

--- seleccionarpuntos.php
+++ seleccionarpuntos.php
@@ -21,7 +21,7 @@ $filtro = ['email' => $Email];
 //$result = $collection2->find(array(), array('projection' => array('nombre' => 'Pascual', '_id' => '633f7bb9543bc9f3d8b635b0')));
 $result = $collection2->find(array('email' => 'adntraining'));
 
-echo 'este es el resultado <br>';
+echo 'este es el resultado ';
 echo $result;
 
 /*
