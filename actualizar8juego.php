dcaf6fc80b6591022590a68c9f0c5178baef36ad
Steven <91740061+Syaga009@users.noreply.github.com>
2022-10-26 20:20:33 -0500

    Create actualizar8juego.php

---
+++ actualizar8juego.php
@@ -0,0 +1,27 @@
+<?php 
+//(ACTUALIZAR)
+header("Access-Control-Allow-Origin: *");
+require_once __DIR__ . '/vendor/autoload.php';
+// Crear Cliente---------------------------------------------------------------------
+$client = new MongoDB\Client('mongodb+srv://TBBLuxari:DMc53jwH5CIQAryP@prueba-puntos.veb9sop.mongodb.net/?retryWrites=true&w=majority');
+
+// Traet Base de datos---------------------------------------------------------------
+$database = $client->Sesiones;
+// Crear o Traer coleccion-----------------------------------------------------------
+$collection2 = $database->usuariostunjas;
+
+$PuntajeUnity = $_GET['Puntos8'];
+$CorreoUnity = $_GET['CorreoUnity'];
+// Actualizar un dato ---------------------------------------------------------------
+
+$filtro = ['email' => $CorreoUnity];
+$update = ['$set' => ['puntos8' => strval($PuntajeUnity)]];
+
+$Actualizar = $collection2->updateOne($filtro,$update);
+
+echo 'Actualizado'
+
+/*
+    Se actualizo o no se actualizo
+*/ 
+?>
